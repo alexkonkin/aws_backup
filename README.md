4c2ae84c0a74032b822db7e346c513135232b6d5
Admin <admin@HRK1-LMC-F73344.local>
2018-11-30 18:47:12 +0200

    backup script has been added to the project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aws_backup
+
+Deploy stack to AWS:
+aws cloudformation create-stack --stack-name ec2-mysql-server --template-body file://ec2.yaml --parameters file://ec2-params.json
+
+Delete stack from AWS:
+aws cloudformation delete-stack --stack-name ec2-mysql-server
+
+
--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #parameters
 stack_name="ec2-mysql-server"
+server_name="MySqlServer"
 bucket_name="alekomail-backup"
+key_name="./MyEc2KeyPair.pem"
+server_ip=""
+server_backup_path="/opt/mysql_backup/*"
+local_backup_path="./store"
 
 #some initial tests that should assure that
 #aws cli is available and can connect to the cloud
@@ -56,9 +61,27 @@ then
     echo "Bucket $bucket_name has been found"
 fi
 
+if [ -z $key_name ]
+then
+    echo "Error:"
+    echo "Key file has not been found $key_name"
+fi
 
+server_ip=$(aws ec2 describe-instances --filter Name="tag:Name,Values=$server_name" --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp]" --output text | xargs | awk -F ' ' '{print $1}')
+if [ `echo $output|wc -c` == 0 ]
+then
+    echo "Error:"
+    echo "Public if of the server with name $server_name has not found"
+    exit 1
+  else
+    echo "Ip of the server $server_name is $server_ip"
+fi
 
+echo "Copy of the files from $server_ip to the $local_backup_path is starting"
+rsync -avz -e "ssh -i $key_name -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ec2-user@$server_ip:$server_backup_path $local_backup_path
 
+echo "Copy of the backup files from $local_backup_path is starting"
+aws s3 sync --delete $local_backup_path s3://$bucket_name/ --delete
 
 
 
